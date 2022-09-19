e5e7a88f368050bd815142d75f7efd758c9d06d3
Abigail Henderson <AGH.coding.2022@gmail.com>
2022-09-19 18:15:10 -0400

    Test 2nd update

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I have been updated!
-        </p>
+        <p>I have been updated again!</p>    
         <a
           className="App-link"
           href="https://reactjs.org"
